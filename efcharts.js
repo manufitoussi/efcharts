fdccc7ceee8af517c64f37f8bdc5213d3f66c185
manufitoussi <manu.fitoussi@gmail.com>
2013-03-25 11:33:01 +0100

    jslint of codes.
    scope enhancement.

--- efcharts.js
+++ efcharts.js
@@ -1,171 +1,176 @@
-EfCharts = function (container, data) {
-  this._init(container, data);
-};
+/*jshint globalstrict: true */
+(function (window) {
+  "use strict";
+  window.EfCharts = function (container, data) {
+    this.init_(container, data);
+  };
 
-EfCharts.VERSION = '0.1';
-EfCharts.NAME = 'EfCharts';
-EfCharts.AUTHOR = 'Emmanuel Fitoussi';
+  EfCharts.VERSION = '0.1';
+  EfCharts.NAME = 'EfCharts';
+  EfCharts.AUTHOR = 'Emmanuel Fitoussi';
 
-EfCharts.DEFAULT_WIDTH = 480;
-EfCharts.DEFAULT_HEIGHT = 320;
+  EfCharts.DEFAULT_WIDTH = 480;
+  EfCharts.DEFAULT_HEIGHT = 320;
 
-EfCharts.prototype._init = function (container, data) {
+  EfCharts.prototype.init_ = function (container, data) {
 
-  if (!container) {
-    // error
-    console.error('container must be set.');
-    return;
-  }
-
-  this._container = container;
-  this._data = data;
-
-  this._parseData();
-  this._preRender();
-  this._render();
-};
-
-EfCharts.prototype.xDomToValue = function (xDom) {
-  console.error('not implemented.');
-  return null;
-};
-
-EfCharts.prototype.yDomToValue = function (yDom) {
-  console.error('not implemented.');
-  return null;
-};
-
-EfCharts.prototype.xValueToDom = function (xValue) {
-  var xDom = 0;
-  var width = EfCharts.DEFAULT_WIDTH;
-  var range = this.getXRange();
-  xDom = (xValue - 0) / (range[1] - range[0]) * width;
-  return xDom;
-};
-
-EfCharts.prototype.yValueToDom = function (yValue) {
-  var yDom = 0;
-  var height = EfCharts.DEFAULT_HEIGHT;
-  var range = this.getYRange();
-  yDom = (1 - (yValue - range[0]) / (range[1] - range[0])) * height;
-  return yDom;
-};
-
-EfCharts.prototype.getXRange = function () {
-  var max = -Infinity;
-  var min = Infinity;
-  var data = this._data;
-
-  for (var i = 0; i < data.length; i++) {
-    min = Math.min(min, data[i][0]);
-    max = Math.max(max, data[i][0]);
-  }
-
-  return [min, max];
-};
-EfCharts.prototype.getYRange = function () {
-  var max = -Infinity;
-  var min = Infinity;
-  var data = this._data;
-
-  for (var i = 0; i < data.length; i++) {
-    for (var j = 1; j < data[0].length; j++) {
-      min = Math.min(min, data[i][j]);
-      max = Math.max(max, data[i][j]);
-    }
-  }
-
-  return [min, max];
-};
-
-EfCharts.isIntNullOrUndefined = function (integer) {
-  return isNaN(parseInt(integer, 10));
-};
-
-EfCharts.prototype._parseData = function () {
-  this._seriesCollection = [];
-  var data = this._data;
-
-  // verify number of columns
-  for (var i = 0; i < data.length; i++) {
-    if (!EfCharts.isIntNullOrUndefined(this._seriesCount) && this._seriesCount !== data[i].length) {
-      console.error('data is not valid. Number of columns are not constant.');
+    if (!container) {
+      // error
+      console.error('container must be set.');
       return;
     }
 
-    this._seriesCount = data[i].length;
-    this._rowsCount = data.length;
-  }
+    this.container_ = container;
+    this.data_ = data;
 
-  var xSeries = { values: [], title: '', axis: 'x' };
-  this._seriesCollection.push(xSeries);
-  // creates x axis values
-  for (i = 0; i < data.length; i++) {
-    xSeries.values.push(data[i][0]);
-  }
+    this.parseData_();
+    this.preRender_();
+    this.render_();
+  };
 
-  // creates seriesCollection.
-  for (var j = 1; j < this._seriesCount; j++) {
-    var series = { values: [], title: '', axis:'y1', context : {} };
-    this._seriesCollection.push(series);
+  EfCharts.prototype.xDomToValue = function (xDom) {
+    console.error('not implemented.');
+    return null;
+  };
+
+  EfCharts.prototype.yDomToValue = function (yDom) {
+    console.error('not implemented.');
+    return null;
+  };
+
+  EfCharts.prototype.xValueToDom = function (xValue) {
+    var xDom = 0;
+    var width = EfCharts.DEFAULT_WIDTH;
+    var range = this.getXRange();
+    xDom = (xValue - range[0]) / (range[1] - range[0]) * width;
+    return xDom;
+  };
+
+  EfCharts.prototype.yValueToDom = function (yValue) {
+    var yDom = 0;
+    var height = EfCharts.DEFAULT_HEIGHT;
+    var range = this.getYRange();
+    yDom = (1 - (yValue - range[0]) / (range[1] - range[0])) * height;
+    return yDom;
+  };
+
+  EfCharts.prototype.getXRange = function () {
+    var max = -Infinity;
+    var min = Infinity;
+    var data = this.data_;
+    var i;
     for (i = 0; i < data.length; i++) {
-      series.values.push(data[i][j]);
+      min = Math.min(min, data[i][0]);
+      max = Math.max(max, data[i][0]);
     }
-  }
 
-  // creates y axes:
-  this._axes = {};
-  var axis = { range: this.getYRange(), title: '' };
-  this._axes.y1 = axis;
+    return [min, max];
+  };
+  EfCharts.prototype.getYRange = function () {
+    var max = -Infinity;
+    var min = Infinity;
+    var data = this.data_;
+    var i, j;
+    for (i = 0; i < data.length; i++) {
+      for (j = 1; j < data[0].length; j++) {
+        min = Math.min(min, data[i][j]);
+        max = Math.max(max, data[i][j]);
+      }
+    }
 
-  // creates absisse:
-  var xAxis = { range: this.getXRange(), title: '' };
-  this._axes.x = xAxis;
+    return [min, max];
+  };
 
-};
+  EfCharts.isIntNullOrUndefined = function (integer) {
+    return isNaN(parseInt(integer, 10));
+  };
 
-EfCharts.prototype._preRender = function () {
-  // delete previous constructions
-  this._container.innerHtml = '';
-  this._canvases = [];
-};
+  EfCharts.prototype.parseData_ = function () {
+    this.seriesCollection_ = [];
+    var data = this.data_;
+    var i, j;
 
-EfCharts.prototype._render = function () {
+    // verify number of columns
+    for (i = 0; i < data.length; i++) {
+      if (!EfCharts.isIntNullOrUndefined(this._seriesCount)
+            && this._seriesCount !== data[i].length) {
+        console.error('data is not valid. Number of columns are not constant.');
+        return;
+      }
 
-  this._container.style.height = EfCharts.DEFAULT_HEIGHT + 'px';
-  this._container.style.width = EfCharts.DEFAULT_WIDTH + 'px';
+      this._seriesCount = data[i].length;
+      this._rowsCount = data.length;
+    }
 
-  for (var j = 1; j < this._seriesCollection.length; j++) {
-    var canvas = document.createElement('canvas');
+    var xSeries = { values: [], title: '', axis: 'x' };
+    this.seriesCollection_.push(xSeries);
+    // creates x axis values
+    for (i = 0; i < data.length; i++) {
+      xSeries.values.push(data[i][0]);
+    }
 
-    canvas.height = EfCharts.DEFAULT_HEIGHT;
-    canvas.width = EfCharts.DEFAULT_WIDTH;
-    canvas.style.position = 'absolute';
+    // creates seriesCollection.
+    for (j = 1; j < this._seriesCount; j++) {
+      var series = { values: [], title: '', axis: 'y1', context : {} };
+      this.seriesCollection_.push(series);
+      for (i = 0; i < data.length; i++) {
+        series.values.push(data[i][j]);
+      }
+    }
 
-    var ctx = canvas.getContext('2d');
+    // creates y axes:
+    this.axes_ = {};
+    var axis = { range: this.getYRange(), title: '' };
+    this.axes_.y1 = axis;
 
-    ctx.lineWidth = 1.0;
-    ctx.lineJoin = 'round';
-    ctx.beginPath();
+    // creates absisse:
+    var xAxis = { range: this.getXRange(), title: '' };
+    this.axes_.x = xAxis;
 
-    // first point
-    var point = {
-      x: this._seriesCollection[0].values[0],
-      y: this._seriesCollection[j].values[0]
-    };
-    ctx.moveTo(this.xValueToDom(point.x), this.yValueToDom(point.y));
-    for (var i = 0; i < this._rowsCount - 1; i++) {
-      point = {
-        x: this._seriesCollection[0].values[i+1],
-        y: this._seriesCollection[j].values[i+1]
+  };
+
+  EfCharts.prototype.preRender_ = function () {
+    // delete previous constructions
+    this.container_.innerHtml = '';
+    this.canvases_ = [];
+  };
+
+  EfCharts.prototype.render_ = function () {
+    var i, j;
+    this.container_.style.height = EfCharts.DEFAULT_HEIGHT + 'px';
+    this.container_.style.width = EfCharts.DEFAULT_WIDTH + 'px';
+
+    for (j = 1; j < this.seriesCollection_.length; j++) {
+      var canvas = document.createElement('canvas');
+
+      canvas.height = EfCharts.DEFAULT_HEIGHT;
+      canvas.width = EfCharts.DEFAULT_WIDTH;
+      canvas.style.position = 'absolute';
+
+      var ctx = canvas.getContext('2d');
+
+      ctx.lineWidth = 1.0;
+      ctx.lineJoin = 'round';
+      ctx.beginPath();
+
+      // first point
+      var point = {
+        x: this.seriesCollection_[0].values[0],
+        y: this.seriesCollection_[j].values[0]
       };
-      ctx.lineTo(this.xValueToDom(point.x), this.yValueToDom(point.y));
+      ctx.moveTo(this.xValueToDom(point.x), this.yValueToDom(point.y));
+      for (i = 0; i < this._rowsCount - 1; i++) {
+        point = {
+          x: this.seriesCollection_[0].values[i + 1],
+          y: this.seriesCollection_[j].values[i + 1]
+        };
+        ctx.lineTo(this.xValueToDom(point.x), this.yValueToDom(point.y));
+      }
+
+      ctx.stroke();
+
+      this.container_.appendChild(canvas);
+      this.canvases_.push(canvas);
     }
-
-    ctx.stroke();
-
-    this._container.appendChild(canvas);
-    this._canvases.push(canvas);
-  }
-};
-
+  };
+}(window));
--- unit_tests/tests.js
+++ unit_tests/tests.js
@@ -1,63 +1,90 @@
-EfCharts.Tests = {};
-EfCharts.Tests.data = [
-      [0, 10, 50, 60],
-      [1, 20, 40, 40],
-      [2, 15, 30, 20],
-      [3, 25, 20, -5]
-];
+/*jshint globalstrict: true */
+/*global EfCharts:false, test:false, ok:false, deepEqual:false,
+  strictEqual:false*/
+(function (window) {
+  "use strict";
 
-test('isIntNullOrUndefined', function () {
-  strictEqual(EfCharts.isIntNullOrUndefined(0), false, '0');
-  strictEqual(EfCharts.isIntNullOrUndefined(0.0), false, '0.0');
-  strictEqual(EfCharts.isIntNullOrUndefined(5), false, '5');
-  strictEqual(EfCharts.isIntNullOrUndefined(5.0), false, '5.0');
-  strictEqual(EfCharts.isIntNullOrUndefined(-4), false, '-4');
-  strictEqual(EfCharts.isIntNullOrUndefined(-4.0), false, '-4.0');
-  strictEqual(EfCharts.isIntNullOrUndefined(NaN), true, 'NaN');
-  strictEqual(EfCharts.isIntNullOrUndefined(null), true, 'null');
-  strictEqual(EfCharts.isIntNullOrUndefined(undefined), true, 'undefined');
-});
+  EfCharts.prototype.getAllSeries = function () {
+    return this.seriesCollection_;
+  };
 
-test('efcharts creation', function () {
-  var div = document.getElementById('efcharts');
-  var charts = new EfCharts(div, EfCharts.Tests.data);
-  ok(charts, 'Created!');
-});
+  EfCharts.prototype.getCanvases = function () {
+    return this.canvases_;
+  };
 
-test('ranges', function () {
-  var div = document.getElementById('efcharts');
-  var charts = new EfCharts(div, EfCharts.Tests.data);
-  var xRange = charts.getXRange();
-  var yRange = charts.getYRange();
-  deepEqual(xRange, [0, 3], 'xRange');
-  deepEqual(yRange, [-5, 60], 'yRange');
+  EfCharts.prototype.getAxes = function () {
+    return this.axes_;
+  };
 
-});
+  EfCharts.Tests = {};
+  EfCharts.Tests.data = [
+    [0, 10, 50, 60],
+    [1, 20, 40, 40],
+    [2, 15, 30, 20],
+    [3, 25, 20, -5]
+  ];
 
-test('xValueToDom', function () {
-  var div = document.getElementById('efcharts');
-  var charts = new EfCharts(div, EfCharts.Tests.data);
-  var xRange = charts.getXRange();
-  for (var x = -2; x < (EfCharts.Tests.data.length + 2) ; x++) {
-    strictEqual(charts.xValueToDom(x), (x - xRange[0]) * 1.0 / (xRange[1] - xRange[0]) * EfCharts.DEFAULT_WIDTH, 'test ' + x);
-  }
-});
+  test('isIntNullOrUndefined', function () {
+    strictEqual(EfCharts.isIntNullOrUndefined(0), false, '0');
+    strictEqual(EfCharts.isIntNullOrUndefined(0.0), false, '0.0');
+    strictEqual(EfCharts.isIntNullOrUndefined(5), false, '5');
+    strictEqual(EfCharts.isIntNullOrUndefined(5.0), false, '5.0');
+    strictEqual(EfCharts.isIntNullOrUndefined(-4), false, '-4');
+    strictEqual(EfCharts.isIntNullOrUndefined(-4.0), false, '-4.0');
+    strictEqual(EfCharts.isIntNullOrUndefined(NaN), true, 'NaN');
+    strictEqual(EfCharts.isIntNullOrUndefined(null), true, 'null');
+    strictEqual(EfCharts.isIntNullOrUndefined(undefined), true, 'undefined');
+  });
 
-test('yValueToDom', function () {
-  var div = document.getElementById('efcharts');
-  var charts = new EfCharts(div, EfCharts.Tests.data);
-  var yRange = charts.getYRange();
-  for (var y = yRange[0]-20; y < yRange[1] + 20; y += 10) {
-    strictEqual(charts.yValueToDom(y), (1- (y - yRange[0]) * 1.0 / (yRange[1] - yRange[0]) )* EfCharts.DEFAULT_HEIGHT, 'test ' + y);
-  }
-});
+  test('efcharts creation', function () {
+    var div = document.getElementById('efcharts');
+    var charts = new EfCharts(div, EfCharts.Tests.data);
+    ok(charts, 'Created!');
+  });
 
-test('series count', function(){
-  var div = document.getElementById('efcharts');
-  var charts = new EfCharts(div, EfCharts.Tests.data);
-  var count = EfCharts.Tests.data[0].lenght;
-  strictEqual(charts._seriesCollection.lenght, count, 'nb of series');
-  strictEqual(charts._canvases.lenght, count, 'nb of canvas');
-  ok(charts._axes.x, 'axis x is present');
-  ok(charts._axes.y1, 'axis y1 is present');
-});
+  test('ranges', function () {
+    var div = document.getElementById('efcharts');
+    var charts = new EfCharts(div, EfCharts.Tests.data);
+    var xRange = charts.getXRange();
+    var yRange = charts.getYRange();
+    deepEqual(xRange, [0, 3], 'xRange');
+    deepEqual(yRange, [-5, 60], 'yRange');
+
+  });
+
+  test('xValueToDom', function () {
+    var div = document.getElementById('efcharts');
+    var charts = new EfCharts(div, EfCharts.Tests.data);
+    var xRange = charts.getXRange();
+    var x;
+    for (x = -2; x < (EfCharts.Tests.data.length + 2); x++) {
+      strictEqual(charts.xValueToDom(x),
+                  (x - xRange[0]) / (xRange[1] - xRange[0])
+                    * EfCharts.DEFAULT_WIDTH,
+                  'test ' + x);
+    }
+  });
+
+  test('yValueToDom', function () {
+    var div = document.getElementById('efcharts');
+    var charts = new EfCharts(div, EfCharts.Tests.data);
+    var yRange = charts.getYRange();
+    var y;
+    for (y = yRange[0] - 20; y < yRange[1] + 20; y += 10) {
+      strictEqual(charts.yValueToDom(y),
+                  (1 - (y - yRange[0]) / (yRange[1] - yRange[0]))
+                    * EfCharts.DEFAULT_HEIGHT,
+                  'test ' + y);
+    }
+  });
+
+  test('series count', function () {
+    var div = document.getElementById('efcharts');
+    var charts = new EfCharts(div, EfCharts.Tests.data);
+    var count = EfCharts.Tests.data[0].lenght;
+    strictEqual(charts.getAllSeries().lenght, count, 'nb of series');
+    strictEqual(charts.getCanvases().lenght, count, 'nb of canvas');
+    ok(charts.getAxes().x, 'axis x is present');
+    ok(charts.getAxes().y1, 'axis y1 is present');
+  });
+}(window));
